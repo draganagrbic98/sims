0777c9fb91adf4c0118cc44e2465f684c7237dc1
petar <petar@DESKTOP-3J5GFJE>
2019-06-30 18:07:41 +0200

    Dodao automatsko ucitavanje default podataka ako ucitavanje podataka iz fajla ne uspe.

--- projekat/src/gui/Main.java
+++ projekat/src/gui/Main.java
@@ -1,7 +1,6 @@
 package gui;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -11,17 +10,37 @@ import model.WebShop;
 
 public class Main {
 
-	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
+	public static void main(String[] args) {
 
 		/*
 		ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("out"));
 		out.writeObject(new WebShop("WAU")); out.flush(); out.close();
 		 */
 		
-		ObjectInputStream in = new ObjectInputStream(new FileInputStream("out"));
-		WebShop ws = (WebShop) in.readObject();
-		in.close();
-
+		ObjectInputStream in = null;
+		WebShop ws = null;
+		
+		
+		// ovo bi trebalo da obezbedi automatsko ucitavanje default podataka
+		//  u slucaju da ucitavanje podataka iz fajla ne uspe
+		while (true) {
+			try {
+				in = new ObjectInputStream(new FileInputStream("out"));
+				ws = (WebShop) in.readObject();
+				in.close();
+				break;
+			} catch (IOException | ClassNotFoundException e) {
+				ObjectOutputStream out;
+				try {
+					out = new ObjectOutputStream(new FileOutputStream("out"));
+					out.writeObject(new WebShop("WAU")); out.flush(); out.close();
+					continue;
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+		}
+		
 		if (!ws.imaAdmina())
 			ws.dodajPodrazumevanogAdmina();
 
